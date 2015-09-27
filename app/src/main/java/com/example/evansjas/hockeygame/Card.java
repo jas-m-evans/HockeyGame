43cb3cd4efdf6c7dbae3cec67e63fe26a325a907
Tom Beck <tom.beck@live.com>
2015-09-26 23:42:33 -0600

    added a Card class

---
+++ app/src/main/java/com/example/evansjas/hockeygame/Card.java
@@ -0,0 +1,21 @@
+package com.example.evansjas.hockeygame;
+
+public class Card {
+    private String name;
+    private String imageFileName;
+
+    public Card() {
+        name = "";
+        imageFileName = "ghost";
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String imageFileName() {
+        return imageFileName;
+    }
+
+
+}
